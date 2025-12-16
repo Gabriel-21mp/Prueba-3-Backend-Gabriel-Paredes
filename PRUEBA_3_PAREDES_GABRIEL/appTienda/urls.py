3b59f35a1442691ac1d769160b623de00c23653b
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-16 17:27:12 -0300

    Codigo tienda prueba 3

---
+++ PRUEBA_3_PAREDES_GABRIEL/appTienda/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.catalogo, name="catalogo"),
+    path("producto/<int:id>/", views.detalle_producto, name="detalle_producto"),
+    path('solicitud/', views.solicitud_pedido, name='solicitud_pedido'),
+    path('seguimiento/<str:token>/', views.detalle_seguimiento, name='detalle_seguimiento'),
+]   
+

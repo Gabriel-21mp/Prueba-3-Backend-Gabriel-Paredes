17c43fda725b62b2a6764b41915c07014eee70d1
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 04:11:44 -0300

    Update views.py

--- PRUEBA_3_PAREDES_GABRIEL/appTienda/api/views.py
+++ PRUEBA_3_PAREDES_GABRIEL/appTienda/api/views.py
@@ -1,7 +1,26 @@
-from rest_framework import viewsets
-from appTienda.models import Insumo
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from appTienda.models import Insumo, Pedido
 from .serializers import InsumoSerializer
+from .serializers import PedidoSerializer
 
 class InsumoViewSet(viewsets.ModelViewSet):
     queryset = Insumo.objects.all().order_by("id")
     serializer_class = InsumoSerializer
+
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all().order_by("-id")
+    serializer_class = PedidoSerializer
+    
+    def list(self, request, *args, **kwargs):
+        return Response(
+            {"detail": "Listado no permitido en este endpoint."},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED
+        )
+    
+    def destroy(self, request, *args, **kwargs):
+        return Response(
+            {"detail": "Eliminación no permitida en este endpoint."},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED
+        )

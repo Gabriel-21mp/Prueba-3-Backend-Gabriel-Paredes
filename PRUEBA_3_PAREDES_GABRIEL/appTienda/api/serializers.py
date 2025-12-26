c4379fc0aad6b0552294d47180dc78bc0d508b5e
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 04:12:07 -0300

    Update serializers.py

--- PRUEBA_3_PAREDES_GABRIEL/appTienda/api/serializers.py
+++ PRUEBA_3_PAREDES_GABRIEL/appTienda/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from appTienda.models import Insumo
+from appTienda.models import Pedido
+
+
 
 class InsumoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Insumo
         fields = "__all__"
+
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = "__all__"
+        read_only_fields = ("token_seguimiento", "fecha_creacion")

1bf717512d442813b740bd1c8d96c44fb98d2b15
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 04:11:57 -0300

    Update urls.py

--- PRUEBA_3_PAREDES_GABRIEL/appTienda/api/urls.py
+++ PRUEBA_3_PAREDES_GABRIEL/appTienda/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import InsumoViewSet
+from .views import InsumoViewSet, PedidoViewSet
 
 router = DefaultRouter()
 router.register(r"insumos", InsumoViewSet, basename="insumos")
+router.register(r"pedidos", PedidoViewSet, basename="pedidos")
 
 urlpatterns = [
     path("", include(router.urls)),
 ]
+
+

7369b86a28540a872875e2291f240fd029c1d5b1
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 02:48:43 -0300

    Update settings.py

--- PRUEBA_3_PAREDES_GABRIEL/PRUEBA_GABRIEL/settings.py
+++ PRUEBA_3_PAREDES_GABRIEL/PRUEBA_GABRIEL/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'cloudinary_storage',
+    'rest_framework',
     'appTienda',
 
 
@@ -169,6 +170,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 

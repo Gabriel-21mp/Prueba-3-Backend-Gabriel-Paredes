fde9de77482034c5b785a1d96cdd3d138661037d
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 02:50:15 -0300

    Update urls.py

--- PRUEBA_3_PAREDES_GABRIEL/PRUEBA_GABRIEL/urls.py
+++ PRUEBA_3_PAREDES_GABRIEL/PRUEBA_GABRIEL/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('appTienda.urls')),
+    path("api/", include("appTienda.api.urls")),
 ]
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

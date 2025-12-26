7d46f933da9df43569431c33f1b0af3cfc27af5b
Gabriel-21mp <paredesandres784@gmail.com>
2025-12-26 05:22:40 -0300

    Update urls.py

--- PRUEBA_3_PAREDES_GABRIEL/appTienda/urls.py
+++ PRUEBA_3_PAREDES_GABRIEL/appTienda/urls.py
@@ -4,9 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.catalogo, name="catalogo"),
     path("producto/<int:id>/", views.detalle_producto, name="detalle_producto"),
-    path('solicitud/', views.solicitud_pedido, name='solicitud_pedido'),
-    path('seguimiento/<str:token>/', views.detalle_seguimiento, name='detalle_seguimiento'),
+    path("solicitud/", views.solicitud_pedido, name="solicitud_pedido"),
+    path("seguimiento/<str:token>/", views.detalle_seguimiento, name="detalle_seguimiento"),
     path("reporte/", views.reporte_pedidos, name="reporte_pedidos"),
-
-]   
-
+]
